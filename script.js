7f2db1a5384e22df353b4d30e8992e3671d8b547
Lindsay Lopez <lopez95@berkeley.edu>
2021-05-31 21:54:25 -0700

    still stying the books

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let bookshelf = document.querySelector('.bookshelf');
 let library = [];
+let bookColors = ['#C33', '#EE8', '#FFC', '#EED', '#F74'];
 
 function Book(title, author, numberOfPages, read) {
   this.title = title;
@@ -17,7 +18,7 @@ function addBookToLibrary(book) {
 }
 
 // Adding a dummy book for now
-let theSecretHistory = new Book('The Secret History', 'Donna Tartt', 537, false);
+let theSecretHistory = new Book('The Secret History verttt long very long name', 'Donna Tartt', 537, false);
 addBookToLibrary(theSecretHistory);
 
 let animalFarm = new Book('Animal Farm', 'George Orwell', 144, true);
@@ -68,9 +69,11 @@ function displayLibrary() {
     newBook.appendChild(newBookDeleteButton);
 
     bookshelf.appendChild(newBook);
+
+    newBook.style.backgroundColor = bookColors[index % (bookColors.length)];
   });
 }
 
 function removeBook(e) {
-  console.log(e.target.dataset.index)
+  console.log(e.target.dataset.index);
 }
