1caf606b8e2b4cf87bda6b5424c0c18f51d1af84
Lindsay Lopez <lopez95@berkeley.edu>
2021-06-01 19:08:54 -0700

    added more styling. Moved header to side

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let bookshelf = document.querySelector('.bookshelf');
+const header = document.querySelector('.header');
+const headerContent = 'LIBRARY';
 let library = [];
 let bookColors = ['#C33', '#EE8', '#FFC', '#EED', '#F74'];
+
+headerContent.split('').forEach(letter => {
+  let p = document.createElement('p');
+  p.textContent = letter;
+  header.appendChild(p);
+});
 
 function Book(title, author, numberOfPages, read) {
   this.title = title;
@@ -18,7 +26,7 @@ function addBookToLibrary(book) {
 }
 
 // Adding a dummy book for now
-let theSecretHistory = new Book('The Secret History verttt long very long name', 'Donna Tartt', 537, false);
+let theSecretHistory = new Book('The Secret History verttt long very long name The Secret History verttt long very long name', 'Donna Tartt he Secret History verttt long very long name The Secret History verttt long very long name', 537, false);
 addBookToLibrary(theSecretHistory);
 
 let animalFarm = new Book('Animal Farm', 'George Orwell', 144, true);
@@ -32,6 +40,8 @@ addBookToLibrary(animalFarm2);
 
 let theSecretHistory23 = new Book('The Secret History', 'Donna Tartt', 537, false);
 addBookToLibrary(theSecretHistory23);
+addBookToLibrary(theSecretHistory);
+addBookToLibrary(theSecretHistory);
 
 displayLibrary();
 
@@ -71,6 +81,7 @@ function displayLibrary() {
     bookshelf.appendChild(newBook);
 
     newBook.style.backgroundColor = bookColors[index % (bookColors.length)];
+    newBook.title = book.title;
   });
 }
 
